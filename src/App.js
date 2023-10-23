5a4e0d3b69e94de14398230dde5ce37d89f2c09a
0nos <phestozonos@gmail.com>
2023-10-23 08:06:55 -0700

    counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const Navbar = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="navbar">
+      <div className="header">Welcome!</div>
+      <div className="nav-links">
+        <a href="/" className="link">
+          Home
         </a>
-      </header>
+        <a href="/counter-app-1" className="link">
+          Counter App 1
+        </a>
+        <a href="/counter-app-2" className="link">
+          Counter App 2
+        </a>
+      </div>
     </div>
   );
-}
+};
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="counter">
+      <div className="counter-buttons">
+        <div className="counter-display">{count}</div>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <Navbar />
+      <Counter />
+    </div>
+  );
+};
 
 export default App;
